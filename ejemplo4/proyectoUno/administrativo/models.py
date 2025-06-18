24cd4005f20982471acb3d8084b8709d81d2f80c
kelvinsrng <sneiderchan67@gmail.com>
2025-06-18 10:30:22 -0500

    commit 4

--- ejemplo4/proyectoUno/administrativo/models.py
+++ ejemplo4/proyectoUno/administrativo/models.py
@@ -2,12 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Pais(models.Model):
-    # nombre
-    # capital
-    # número de provincias
-    # número de habitantes
-    pass
+    nombre = models.CharField(max_length=30)
+    capital = models.CharField(max_length=30)
+    num_provincias = models.IntegerField()
+    num_habitantes = models.IntegerField()
 
+    def __str__(self):
+        return "%s %s %d %d" % (self.nombre,
+                self.capital,
+                self.num_provincias,
+                self.num_habitantes)
 
 class Estudiante(models.Model):
     nombre = models.CharField(max_length=30)
@@ -18,6 +22,7 @@ class Estudiante(models.Model):
         return "%s %s %s" % (self.nombre,
                 self.apellido,
                 self.cedula)
+
 
 class NumeroTelefonico(models.Model):
     telefono = models.CharField(max_length=100)
